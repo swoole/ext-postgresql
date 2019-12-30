4a8474821044c1cd265b0ec4682f3a0b3b854a93
twosee <twose@qq.com>
2019-12-30 13:46:28 +0800

    Update version for Swoole-Ext 4.4.14

--- swoole_postgresql_coro.h
+++ swoole_postgresql_coro.h
@@ -20,8 +20,8 @@
 #include "ext/swoole/php_swoole_cxx.h"
 #include "config.h"
 
-#define PHP_SWOOLE_EXT_POSTGRESQL_VERSION     "4.4.13"
-#define PHP_SWOOLE_EXT_POSTGRESQL_VERSION_ID  40413
+#define PHP_SWOOLE_EXT_POSTGRESQL_VERSION     "4.4.14"
+#define PHP_SWOOLE_EXT_POSTGRESQL_VERSION_ID  40414
 
 #if PHP_SWOOLE_EXT_POSTGRESQL_VERSION_ID != SWOOLE_VERSION_ID
 #error "Ext version does not match the Swoole version"
