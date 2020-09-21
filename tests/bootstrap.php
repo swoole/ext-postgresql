f057856f0028ef66b646d617771d3b2134b985f9
matyhtf <mikan.tenny@gmail.com>
2020-09-21 10:33:34 +0800

    fix tests[3]

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,3 +1,8 @@
 <?php
-
-define('TEST_DB_URI', 'host=127.0.0.1;port=5432;dbname=test;user=postgres;password=postgres');
+if (getenv('POSTGRES_HOST')) {
+    $host = getenv('POSTGRES_HOST');
+    $port = getenv('POSTGRES_PORT');
+    define('TEST_DB_URI', "host={$host};port={$port};dbname=postgres;user=postgres;password=postgres");
+} else {
+    define('TEST_DB_URI', 'host=127.0.0.1;port=5432;dbname=test;user=postgres;password=postgres');
+}
