c8c76193dcf3fcce4d06e149fcdd833b5af31f5b
Yurun <admin@yurunsoft.com>
2021-07-28 13:18:49 +0800

    Fix query results without field names

--- tests/unit/PostgreSQLTest.php
+++ tests/unit/PostgreSQLTest.php
@@ -1,15 +1,17 @@
 <?php
 
+use PHPUnit\Framework\TestCase;
 use Swoole\Coroutine\PostgreSQL;
 use function Swoole\Coroutine\run;
-use PHPUnit\Framework\TestCase;
 
 class PostgreSQLTest extends TestCase
 {
-    protected function getConn() {
+    protected function getConn()
+    {
         $pg = new Swoole\Coroutine\PostgreSQL();
         $conn = $pg->connect(TEST_DB_URI);
-        $this->assertNotFalse($conn, (string)$pg->error);
+        $this->assertNotFalse($conn, (string) $pg->error);
+
         return $pg;
     }
 
@@ -18,7 +20,7 @@ class PostgreSQLTest extends TestCase
         run(function () {
             $pg = $this->getConn();
             $result = $pg->escape("' or 1=1 & 2");
-            $this->assertNotFalse($result, (string)$pg->error);
+            $this->assertNotFalse($result, (string) $pg->error);
             $this->assertEquals("'' or 1=1 & 2", $result);
         });
     }
@@ -29,7 +31,7 @@ class PostgreSQLTest extends TestCase
             $pg = $this->getConn();
             $result = $pg->query("INSERT INTO weather(city, temp_lo, temp_hi, prcp, date) 
                         VALUES ('Shanghai', 88, 10, 0.75,'1993-11-27')  RETURNING id");
-            $this->assertNotFalse($result, (string)$pg->error);
+            $this->assertNotFalse($result, (string) $pg->error);
             $this->assertEquals(1, $pg->numRows($result));
             $this->assertGreaterThan(1, $pg->fetchAssoc($result)['id']);
         });
@@ -40,11 +42,25 @@ class PostgreSQLTest extends TestCase
         run(function () {
             $pg = $this->getConn();
             $result = $pg->query('SELECT * FROM weather;');
-            $this->assertNotFalse($result, (string)$pg->error);
+            $this->assertNotFalse($result, (string) $pg->error);
 
             $arr = $pg->fetchAll($result);
             $this->assertIsArray($arr);
             $this->assertEquals($arr[0]['city'], 'San Francisco');
+        });
+    }
+
+    public function testNoFieldName()
+    {
+        run(function () {
+            $pg = $this->getConn();
+            $result = $pg->query('SELECT 11, 22');
+            $this->assertNotFalse($result, (string) $pg->error);
+
+            $arr = $pg->fetchAll($result);
+            $this->assertIsArray($arr);
+            $this->assertEquals($arr[0]['?column?'], 11);
+            $this->assertEquals($arr[0]['?column?1'], 22);
         });
     }
 }
